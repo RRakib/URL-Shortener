5fcb7d15b22ffbe2dbeb33cc5518790564360378
Rakib_Penta <rakib@pentabd.com>
2020-07-24 12:57:21 +0600

    Setting up the api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let urlHolder = [];
+let aliasHolder = [];
+
+router.get('/:id', function(req, res, next) {
+  let ind = aliasHolder.indexOf(req.params.id);
+  res.redirect(urlHolder[ind]);
+});
+
+router.post('/url', function(req, res, next) {
+  const {url, alias} = req.body;
+  urlHolder.push(url);
+  aliasHolder.push(alias);
 });
 
 module.exports = router;
